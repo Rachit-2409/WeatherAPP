11ce536c4966b9eedfc6755d1889e2be09e6ba53
Rachit-2409 <108273325+Rachit-2409@users.noreply.github.com>
2023-06-15 22:37:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,19 +26,19 @@ async function checkWeather(City){
 
     switch(weather.weather[0].main){
         case 'Clouds' :
-           weather_img.src = "/assets/cloud.png";
+           weather_img.src = "assets/cloud.png";
         break;
         case 'Clear' :
-            weather_img.src = "/assets/clear.png";
+            weather_img.src = "assets/clear.png";
         break;
         case 'Rain' :
-           weather_img.src = "/assets/rain.png";
+           weather_img.src = "assets/rain.png";
         break;
         case 'Haze' :
-           weather_img.src = "/assets/mist.png";
+           weather_img.src = "assets/mist.png";
         break;
         case 'Snow' :
-           weather_img.src = "/assets/snow.png";
+           weather_img.src = "assets/snow.png";
         break;
 
     }
